41635e48b5548cdb4212fdc354dca22430d2f2b5
asfrolov <frolandy@mail.ru>
2019-07-08 19:21:11 +0300

    delete unnecessary

--- src/main/java/jlauncher/master/GUI/MainStage.java
+++ src/main/java/jlauncher/master/GUI/MainStage.java
@@ -14,28 +14,14 @@ public class MainStage {
 
     private double windowHeight = 500;
     private double windowWidth = 695;
-    private Stage stage = new BaseStage(windowWidth, windowHeight);
+    private BaseStage stage = new BaseStage(windowWidth, windowHeight);
     private LauncherMenu menu = new LauncherMenu();
 
     public MainStage(){
-        stage.setTitle("Launcher");
+        stage.setTitle("JLauncher");
         stage.setOnCloseRequest(e -> System.exit(0));
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/icon.png")));
-        stage.setMinWidth(windowWidth);
-        stage.setMinHeight(windowHeight);
-        stage.setWidth(windowWidth);
-        stage.setHeight(windowHeight);
-
         VBox vbox = new VBox(menu.getMenuBar(this));
-        vbox.setBackground(new Background(new BackgroundFill(Color.web("#4C4C4C"), CornerRadii.EMPTY, Insets.EMPTY)));
-
-        Scene scene = new Scene(vbox);
-        scene.setFill(Color.TRANSPARENT);
-        scene.getStylesheets().add(getClass().getResource("styles/style.css").toExternalForm());
-
-        stage.setScene(scene);
-
-        stage.show();
+        stage.setRoot(vbox);
     }
 
     Stage getPrimaryStage() { return this.stage; }
