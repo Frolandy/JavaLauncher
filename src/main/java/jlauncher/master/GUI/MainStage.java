119fd2e339eb24454afe2d661550f982eab5d3b7
asfrolov <frolandy@mail.ru>
2019-07-08 19:21:34 +0300

    refactor imports

--- src/main/java/jlauncher/master/GUI/MainStage.java
+++ src/main/java/jlauncher/master/GUI/MainStage.java
@@ -1,13 +1,6 @@
 package jlauncher.master.GUI;
 
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MainStage {
